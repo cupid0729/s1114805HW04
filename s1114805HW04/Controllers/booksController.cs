3f86d559169e7f37679be863da1407afe720d9a3
cjcbo <cupid0729gsno@gmail.com>
2024-10-13 23:54:51 +0800

    加入專案檔案。

---
+++ s1114805HW04/Controllers/booksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using s1114805HW04.Models;
+
+namespace s1114805HW04.Controllers
+{
+    public class booksController : Controller
+    {
+        private booksEntities db = new booksEntities();
+
+        // GET: books
+        public ActionResult Index()
+        {
+            return View(db.books.ToList());
+        }
+
+        // GET: books/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            books books = db.books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // GET: books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: books/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookId,Name,Author,Painter,Episode")] books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.books.Add(books);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(books);
+        }
+
+        // GET: books/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            books books = db.books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: books/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookId,Name,Author,Painter,Episode")] books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(books).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(books);
+        }
+
+        // GET: books/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            books books = db.books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            books books = db.books.Find(id);
+            db.books.Remove(books);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ s1114805HW04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="s1114805HW04.MvcApplication" Language="C#" %>
